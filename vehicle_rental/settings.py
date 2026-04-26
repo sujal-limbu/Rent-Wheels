045ef8f8966f5648fe9330f5cc93c94ce5a3647a
Sujal <sujallimbu31@gmail.com>
2026-04-26 13:26:21 +0545

    changed database

--- vehicle_rental/settings.py
+++ vehicle_rental/settings.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 import os
 from dotenv import load_dotenv
+import dj_database_url
+
 
 load_dotenv()
 
@@ -79,10 +81,10 @@ TEMPLATES = [
 WSGI_APPLICATION = 'vehicle_rental.wsgi.application'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f'sqlite:///{BASE_DIR / "db.sqlite3"}',
+        conn_max_age=600,
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = [
