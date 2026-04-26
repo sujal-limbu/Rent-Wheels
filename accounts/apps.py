d95b8cb9d5cd7332b1740067cf104c25de5f17bf
Sujal <sujallimbu31@gmail.com>
2026-04-26 12:57:08 +0545

    More updates for authentication

--- accounts/apps.py
+++ accounts/apps.py
@@ -6,4 +6,13 @@ class AccountsConfig(AppConfig):
     name = 'accounts'
 
     def ready(self):
-        import accounts.signals  
+        import accounts.signals
+
+        try:
+            from django.contrib.sites.models import Site
+            Site.objects.filter(id=1).update(
+                domain='rent-wheels-i6qi.onrender.com',
+                name='RentWheels'
+            )
+        except Exception:
+            pass
